0fb612987b79addde4d682b607d7749628239684
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-04 23:28:50 +0100

    feat: Add TypeScript interfaces for API requests

--- src/interfaces/Http.ts
+++ src/interfaces/Http.ts
@@ -1,15 +1,9 @@
-// export enum HttpRequestMethod {
-//     GET = 'GET',
-//     HEAD = 'HEAD',
-//     POST = 'POST',
-//     PUT = 'PUT',
-//     DELETE = 'DELETE',
-//     OPTIONS = 'OPTIONS',
-//     PATCH = 'PATCH'
-// };
-
-export type HttpRequestMethod = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'OPTIONS' | 'PATCH';
-
-export type HttpRequestParams = {
-    [key: string]: string | number | boolean
+export enum Http {
+    GET = 'GET',
+    HEAD = 'HEAD',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    OPTIONS = 'OPTIONS',
+    PATCH = 'PATCH'
 };
