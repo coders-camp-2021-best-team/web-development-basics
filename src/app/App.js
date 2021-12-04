8b4ae1dc9802c6cb5ed90d95288e8788b7158b53
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-04 23:30:16 +0100

    fix: Update example usage in `App` component

--- src/app/App.js
+++ src/app/App.js
@@ -8,7 +8,13 @@ const template = `
 export const App = ({ renderOn }) => {
     render({ on: renderOn, html: template });
 
-    ApiProvider.search('Minions')
-        .then((v) => console.debug(v))
+    ApiProvider.search('Fast & Furious')
+        .then((v) => {
+            console.debug(v);
+
+            v.results.forEach(async (result) => {
+                await ApiProvider.getTitleDetails(result.id);
+            });
+        })
         .catch(console.error);
 };
