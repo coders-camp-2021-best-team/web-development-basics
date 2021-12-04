6819cd4deb38bd20dea5b00bc205673f4557027a
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-04 13:38:17 +0100

    feat: Add example `ApiService` usage to `App.js`
    !!! This is temporary just to check if ApiService works. !!!
    You will not use this class directly!
    There will be another class ApiProvider that you will work with.

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,17 @@
 import { render } from '../shared/dom';
+import apiService from '../services/ApiService';
 
 const template = `
     <div>Hello world!</div>
 `;
 
-export const App = ({ renderOn, options }) => {
-  render({ on: renderOn, html: template });
+export const App = ({ renderOn }) => {
+    render({ on: renderOn, html: template });
+
+    apiService
+        .request('GET', '/title/find', {
+            q: 'Avengers'
+        })
+        .then((v) => console.log(JSON.stringify(v)))
+        .catch(console.error);
 };
