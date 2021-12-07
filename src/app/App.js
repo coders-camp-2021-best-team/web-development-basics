77ac32d7c6c164724072bd2ef1b7ea37cc1e0113
wiktbro824 <wiktbro824@student.polsl.pl>
2021-12-07 22:19:15 +0100

    Move router from App to BaseScreen

--- src/app/App.js
+++ src/app/App.js
@@ -1,33 +1,12 @@
 import { render } from '../shared/dom';
-import { Router, routes } from '../route';
+import { BaseScreen } from '../screens';
 
 const template = `
-    <div>
-        <main>
-            <template id="app-main"></template>
-        </main>
-    </div>
+    <template id="app-basescreen"></template>
 `;
 
 export const App = ({ renderOn }) => {
     render({ html: template, on: renderOn }, false);
 
-    Router({
-        // TODO: add your components
-        routes: [
-            {
-                component: () => null,
-                route: routes.home
-            }
-            // {
-            //     component: SearchScreen,
-            //     route: routes.search
-            // },
-            // {
-            //     component: Error404Screen,
-            //     route: routes.error404
-            // }
-        ],
-        on: '#app-main'
-    });
+    BaseScreen({ renderOn: '#app-basescreen' });
 };
