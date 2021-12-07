0e7c5fdc758fdf088ef6a2926cc0406ccf46e4b1
Konrad <trakon1e@gmail.com>
2021-12-07 09:30:36 +0100

    Add getItem method to StorageManager

--- src/services/StorageManager.js
+++ src/services/StorageManager.js
@@ -1,10 +1,14 @@
 export class StorageManager  {
-    addAsset(name, myObj) {
-      let myObjToString = JSON.stringify(myObj)
-      localStorage.setItem(name, myObjToString)
-     }
-     
-    removeAsset(name) {
-       localStorage.removeItem(name)
-     }
- } 
+  addItem(name, myObj) {
+    let myObj_serialized = JSON.stringify(myObj)
+    localStorage.setItem(name, myObj_serialized)
+   }
+   
+  removeItem(name) {
+     localStorage.removeItem(name)
+   }
+  getItem(name) {
+      let myObj_deserialized = JSON.parse(localStorage.getItem(name))
+      return myObj_deserialized
+   }
+} 
