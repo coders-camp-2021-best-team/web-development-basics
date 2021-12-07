4c6a54b67f322717ffd49023ca1922925d25f471
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-07 22:31:19 +0100

    refactor: Format all files using Prettier
    `npm run format`

--- src/services/StorageManager.js
+++ src/services/StorageManager.js
@@ -1,15 +1,15 @@
 class StorageManager {
-  addItem(name, myObj) {
-    const myObj_serialized = JSON.stringify(myObj);
-    localStorage.setItem(name, myObj_serialized);
-  } 
-  removeItem(name) {
-    localStorage.removeItem(name);
-  }
-  getItem(name) {
-    const myObj_deserialized = JSON.parse(localStorage.getItem(name));
-    return myObj_deserialized;
-  }
-} 
+    addItem(name, myObj) {
+        const myObj_serialized = JSON.stringify(myObj);
+        localStorage.setItem(name, myObj_serialized);
+    }
+    removeItem(name) {
+        localStorage.removeItem(name);
+    }
+    getItem(name) {
+        const myObj_deserialized = JSON.parse(localStorage.getItem(name));
+        return myObj_deserialized;
+    }
+}
 
 export default new StorageManager();
