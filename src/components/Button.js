f276ce4cd8c4295ba68af930e206258c76acd0e0
wiktbro824 <wiktbro824@student.polsl.pl>
2021-12-08 22:38:05 +0100

    feat: Add onClick function, new extra class and button type

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
-import { render } from "../shared/dom";
+import { render } from '../shared/dom';
 
-const template = (text) => `
-    <button class='btn>${text}</button>
+
+
+const template = (className, onClick, text, type) => `
+    <button class='btn ${className}' type="${type}" onClick="${onClick()}">${text}</button>
 `;
 
-export const Button = ({renderOn, text}) => {
-    render({html:template(text), on: renderOn});
-}
+export const Button = ({ className, onClick, text, type }) => {
+    render({ html: template(className, onClick, text, type), on: renderOn });
+};
