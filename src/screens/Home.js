8fbc4fbc1be2cf872a95df6faa4439f4bde81d0f
N00BKU <norbert.jankowski.akademia@gmail.com>
2021-12-08 19:36:00 +0100

    editHomscreen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,18 @@
 import { render } from '../shared/dom.js';
+import { MovieTiles } from '../components';
+import ApiProvider from '../providers/ApiProvider.js';
 
 const template = `
-    <div id="home-screen">This is home screen</div>
+    <div>
+        <div id="home-screen">This is home screen</div>
+        <template id="home-movie-tiles"></template>
+    </div>
 `;
 
-export const HomeScreen = ({ renderOn, options }) => {
+export const HomeScreen = async ({ renderOn, options }) => {
     render({on: renderOn, html: template});
+
+    const movies = await ApiProvider.search('Inception');
+
+    MovieTiles({ renderOn: '#home-movie-tiles', movies: movies.results });
 };
