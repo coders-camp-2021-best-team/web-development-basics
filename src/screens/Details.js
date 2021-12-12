6d8a9ba3588c3dbce8050a8afb6cd3b570ef6b84
Konrad <trakon1e@gmail.com>
2021-12-12 22:24:51 +0100

    Add styling details-screen

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,10 +1,10 @@
 import { render } from "../shared/dom.js";
 const template = `
-<div id = "details-screen">
-    <template></template>
-    <template></template>
-    <template></template>
-    <template></template>
+<div id ="details-screen" class="details">
+    <template class="details__logo"></template>
+    <template class="details__trailer"></template>
+    <template class="details__gallery"></template>
+    <template class="details__footer"></template>
 </div>
 `;
 
