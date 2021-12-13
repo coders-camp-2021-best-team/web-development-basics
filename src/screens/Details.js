ee81e21bef36b04cedc6b518d7f44a99563e1a50
Konrad <trakon1e@gmail.com>
2021-12-13 21:32:13 +0100

    refactor: Rename classes

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,10 +1,10 @@
 import { render } from "../shared/dom.js";
 const template = `
 <div id ="details-screen" class="details">
-    <template class="details__logo"></template>
     <template class="details__trailer"></template>
+    <template class="details__poster"></template>
+    <template class="details__gallery-button"></template>
     <template class="details__gallery"></template>
-    <template class="details__footer"></template>
 </div>
 `;
 
