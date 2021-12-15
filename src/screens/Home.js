b7737a4bebe6f07925da20ca56a8fa56cfeb030a
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-15 11:35:36 +0100

    feat: Add example usage to `HomeScreen`

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,23 @@
 import { render } from '../shared/dom.js';
-import { Star } from '../components/index.js';
+import { Star, AssetDetailsDescription } from '../components/index.js';
+import ApiProvider from '../providers/ApiProvider';
 
 const template = `
-    <div id="home-screen">This is home screen</div>
+    <div id="home-screen">
+        This is home screen
+        <template id="desc"></template>
+    </div>
 `;
 
 export const HomeScreen = ({ renderOn, options }) => {
-    render({on: renderOn, html: template});
+    render({ on: renderOn, html: template });
+
+    ApiProvider.search('Fast & Furious').then(async ({ results }) => {
+        const more_details = await ApiProvider.getTitleDetails(results[0].id);
+
+        AssetDetailsDescription({
+            renderOn: '#desc',
+            movie: more_details
+        });
+    });
 };
