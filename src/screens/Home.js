27cdbd0c80eb7f9f24286ac2fd7ec3edad5b8e20
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-15 11:35:45 +0100

    Revert "feat: Add example usage to `HomeScreen`"
    This reverts commit b7737a4bebe6f07925da20ca56a8fa56cfeb030a.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,23 +1,10 @@
 import { render } from '../shared/dom.js';
-import { Star, AssetDetailsDescription } from '../components/index.js';
-import ApiProvider from '../providers/ApiProvider';
+import { Star } from '../components/index.js';
 
 const template = `
-    <div id="home-screen">
-        This is home screen
-        <template id="desc"></template>
-    </div>
+    <div id="home-screen">This is home screen</div>
 `;
 
 export const HomeScreen = ({ renderOn, options }) => {
-    render({ on: renderOn, html: template });
-
-    ApiProvider.search('Fast & Furious').then(async ({ results }) => {
-        const more_details = await ApiProvider.getTitleDetails(results[0].id);
-
-        AssetDetailsDescription({
-            renderOn: '#desc',
-            movie: more_details
-        });
-    });
+    render({on: renderOn, html: template});
 };
