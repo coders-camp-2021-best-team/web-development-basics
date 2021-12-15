d0995658b8320e2ae88ec7f1d045244c2e83bfc6
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-15 20:11:13 +0100

    feat: Add console logging solution (#28)

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,9 @@
 import { render } from '../shared/dom.js';
-import { Star } from '../components/index.js';
 
 const template = `
     <div id="home-screen">This is home screen</div>
 `;
 
 export const HomeScreen = ({ renderOn, options }) => {
-    render({on: renderOn, html: template});
+    render({ on: renderOn, html: template });
 };
