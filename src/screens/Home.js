0be6bf83a147931517d976f02cf00c3bb8e050ee
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-15 12:02:12 +0100

    fix: Remove unnecessary imports

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,4 @@
 import { render } from '../shared/dom.js';
-import { Star } from '../components/index.js';
-import ApiProvider from '../providers/ApiProvider';
-import Logger from '../utils/ConsoleLogger';
 
 const template = `
     <div id="home-screen">This is home screen</div>
