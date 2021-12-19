79ce2aecfef91d3edae70282920ea54fac67b7b2
T0ffik <t0fffik2000@gmail.com>
2021-12-19 11:39:19 +0100

    refactor: correct comments

--- src/components/Tiles/MovieTile.js
+++ src/components/Tiles/MovieTile.js
@@ -1,16 +1,17 @@
 import { Star } from "../Star/Star";
+import { ImageComponent } from "../ImageComponent/ImageComponent";
 
 export const MovieTile = ({ movie }) => {
     return `
     <div class="movie-tile">
         <div class="img-hover">
-            <img src="${movie.image}" class="img-tile"/>
+            ${Star({options : {id: `${movie.id}`}})}
+            ${ImageComponent(`${movie.image}`, 'img-tile', `${movie.id}`)}
             <div class="movie-details">
                 <p><strong>Year:</strong> ${movie.year}</p>
                 <p><strong>Starring:</strong> ${movie.stars}</p>
                 <p><strong>Plot:</strong> ${movie.plot}</p>
             </div>
-            ${Star({options : {id: `${movie.id}`}})}
         </div>
         <h3 class="title">${movie.title}</h3>
     </div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,6 @@
 import { render } from '../shared/dom.js';
 import { MovieCarousel } from '../components';
 import ApiProvider from '../providers/ApiProvider.js';
-import { ImageComponent } from '../components/ImageComponent/ImageComponent.js';
 
 const template = (movies) => `
     <div>
