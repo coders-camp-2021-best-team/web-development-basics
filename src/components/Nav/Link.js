9e9e62b88ad6e85e7acaf2043068c8a7fede8372
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-20 21:23:19 +0100

    feat: Add `NavLink` component

---
+++ src/components/Nav/Link.js
@@ -0,0 +1,3 @@
+export const NavLink = ({ href, title }) => {
+    return `<li><a href="${href}">${title}</a></li>`;
+};
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export * from './Footer/Footer';
 export * from './Button/Button';
 export * from './Tiles/MovieTile';
 export * from './Star/Star';
+export * from './Nav/Link';
