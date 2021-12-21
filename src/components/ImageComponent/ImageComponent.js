795563d5b920b4ef8897a3e38f50a39995ee45e5
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2021-12-21 21:31:22 +0100

    changed code in image placeholder

--- src/components/ImageComponent/ImageComponent.js
+++ src/components/ImageComponent/ImageComponent.js
@@ -1,12 +1,7 @@
 import { observerListener } from '../../shared/observer';
 
 const template = (imageUrl, className, id) => {
-  let src = true;
-  if (src) {
-    return <img src="${imageUrl}" class="${className}" id="${id}"/>
-  } else {
-    return <img src="/static/img/nopicture-min.jpg">
-  };
+  return `<img src=${imageUrl || '/static/img/no-picture-min.jpg'} class="${className}" id="${id}" />`;
 };
 export const ImageComponent = (imageUrl, className, id, callback) => {
     observerListener(id, callback);
