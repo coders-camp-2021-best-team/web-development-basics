f46aa0c18f3cb974745ed82bfe5c1ed4346e85e1
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2021-12-21 20:12:30 +0100

    add placeholder for an image

--- src/components/ImageComponent/ImageComponent.js
+++ src/components/ImageComponent/ImageComponent.js
@@ -1,11 +1,14 @@
 import { observerListener } from '../../shared/observer';
 
 const template = (imageUrl, className, id) => {
-  return `
-    <img src="${imageUrl}" class="${className}" id="${id}"/>
-  `
+  let src = true;
+  if (src) {
+    return <img src="${imageUrl}" class="${className}" id="${id}"/>
+  } else {
+    return <img src="/static/img/nopicture-min.jpg">
+  };
 };
 export const ImageComponent = (imageUrl, className, id, callback) => {
-  observerListener(id, callback);
-  return template (imageUrl, className, id)
+    observerListener(id, callback);
+    return template(imageUrl, className, id);
 };
