c0c44451fdfb2f1ddd50f043b120db72eda73ae6
T0ffik <t0fffik2000@gmail.com>
2021-12-21 11:23:47 +0100

    refactor: Make random movie button work

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,30 +2,43 @@ import { render } from '../../shared/dom';
 import { NavLink } from '../Nav/Link';
 import { routes } from '../../route';
 import { Button } from '../Button/Button';
+import ApiProvider from '../../providers/ApiProvider';
+import { redirect } from '../..';
 
-const template = `
-    <header>
-      <a href="${routes.home.routerPath}">
-        <img src="/static/img/logo.png" alt="logo" class="logo">
-      </a>
-      <div class="navBar">
-        <ul id="menu" class="menu">
-          ${NavLink({ href: '#', title: 'Movie' })}
-          ${NavLink({ href: '#', title: 'TV Series' })}
-          ${NavLink({ href: routes.search.routerPath, title: 'Search' })}
-          ${NavLink({ href: routes.favorites.routerPath, title: 'Favorite' })}
-          <button id="exit" class="exit"><li>&times;</li></button>
-        </ul>
-        <div class="btns">
-          <button id="icon" class="icon"><img src="/static/img/menuIcon.png" alt="toggleMenuIconMinor"></button>
-          ${Button(() => console.log("dupa"), "randomBtn", "random movies", "button" , "randomBtn")}
-        </div>
+const template = (randomMovie) => {
+    return `
+  <header>
+    <a href="${routes.home.routerPath}">
+      <img src="/static/img/logo.png" alt="logo" class="logo">
+    </a>
+    <div class="navBar">
+      <ul id="menu" class="menu">
+        ${NavLink({ href: '#', title: 'Movie' })}
+        ${NavLink({ href: '#', title: 'TV Series' })}
+        ${NavLink({ href: routes.search.routerPath, title: 'Search' })}
+        ${NavLink({ href: routes.favorites.routerPath, title: 'Favorite' })}
+        <button id="exit" class="exit"><li>&times;</li></button>
+      </ul>
+      <div class="btns">
+        <button id="icon" class="icon"><img src="/static/img/menuIcon.png" alt="toggleMenuIconMinor"></button>
+        ${Button(
+            () => {
+                redirect(routes.details.getPathWithParams(randomMovie.id));
+            },
+            'randomBtn',
+            'random movies',
+            'button',
+            'randomBtn'
+        )}
       </div>
-    </header>
+    </div>
+  </header>
 `;
+};
 
-export const Header = ({ renderOn }) => {
-    render({ on: renderOn, html: template });
+export const Header = async ({ renderOn }) => {
+    const randomMovie = await ApiProvider.getRandomAsset();
+    render({ on: renderOn, html: template(randomMovie) });
 
     const open = () => {
         document.getElementById('menu').classList.add('menu-active');
