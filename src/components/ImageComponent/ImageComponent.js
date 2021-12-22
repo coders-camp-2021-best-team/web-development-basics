f023fd9c69d5d644b5c0acb0908fd450fb0d08c9
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2021-12-22 19:22:30 +0100

    img placeholder work :)

--- src/components/ImageComponent/ImageComponent.js
+++ src/components/ImageComponent/ImageComponent.js
@@ -1,9 +1,12 @@
 import { observerListener } from '../../shared/observer';
 
 const template = (imageUrl, className, id) => {
-  return `<img src=${imageUrl || '/static/img/no-picture-min.jpg'} class="${className}" id="${id}" />`;
+    return `<img src = "${imageUrl}"
+    onerror="this.onerror=null;this.src='https://imdb-api.com/images/original/nopicture.jpg';" class="${className}" id="${id}" "/>`;
 };
+
 export const ImageComponent = (imageUrl, className, id, callback) => {
     observerListener(id, callback);
+
     return template(imageUrl, className, id);
 };
