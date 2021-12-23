1ff691783663fc4e6445bb15e01522aac4e86b28
T0ffik <t0fffik2000@gmail.com>
2021-12-23 01:20:03 +0100

    feat: Add CategoryScreen

---
+++ src/screens/Category.js
@@ -0,0 +1,9 @@
+import { Label } from '../components/Label/Label';
+import { TilesGrid } from '../components/Tiles/TilesGrid';
+
+export const Category = () =>{
+  return `
+    ${Label}
+    ${TilesGrid}
+  `;
+};
