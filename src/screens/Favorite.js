1277dbecfb1946140ff26bf5d2f8f3deed758e6a
Konrad <trakon1e@gmail.com>
2021-12-28 19:18:40 +0100

    refactor: Add grid component to favorite-screen

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,11 +1,15 @@
 import StorageManager from '../services/StorageManager';
+import { TilesGrid } from '../components/Tiles/TilesGrid';
 import { render } from '../shared/dom';
 
 const template = `
-    <div></div>
+    <div>
+        <template id="grid-template"></template>
+    </div>
 `;
 
 export const FavoriteScreen = ({ renderOn }) => {
-    render({ html: template, on: renderOn });
     const favorites = StorageManager.getItem('favorites') || [];
+    render({ html: template, on: renderOn });
+    TilesGrid({ renderOn: '#grid-template', favorites });
 };
