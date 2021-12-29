ca1b4bb369315ef835897c2836e5d9d9a5300295
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2021-12-29 19:17:47 +0100

    changed trailder code and add scss to trailer

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,11 +1,11 @@
 import { AssetDetailsDescription } from '../components/AssetDetails/Description.js';
 import { render } from '../shared/dom.js';
 import ApiProvider from '../providers/ApiProvider';
-import { TrailerMovie } from '../components/Trailer/Trailer.js';
+import './Details.scss';
 
 const template = (trailer) => `
 <div id="details-screen" class="details">
-    <template class="details__trailer">${trailer}</template>
+    <div class="details__trailer">${trailer}</div>
     <template class="details__poster"></template>
     <template class="details__description"></template>
     <template class="details__gallery-button"></template>
@@ -19,8 +19,8 @@ export const DetailsScreen = async ({ renderOn }) => {
     const movie = await ApiProvider.getTitleDetails(searchID);
 
     let trailer = '';
-    if (movie.trailer && movie.trailer.link) {
-        trailer = TrailerMovie(movie.trailer.link);
+    if (movie.trailer && movie.trailer.linkEmbed) {
+        trailer = `<iframe class="iframe" src="${movie.trailer.linkEmbed}"></iframe>`;
     }
 
     render({ on: renderOn, html: template(trailer) });
