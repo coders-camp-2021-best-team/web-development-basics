7be72235edcb43e793a3674d1457d94979f497ef
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2021-12-29 16:17:18 +0100

    trailer fixes

--- src/components/Trailer/Trailer.js
+++ src/components/Trailer/Trailer.js
@@ -1,17 +1,23 @@
+import videojs from 'video.js';
+
+const template = (videoUrl) => {
+    return `
+    <video
+    id="MY_VIDEO_ID"
+    class="video-js vjs-default-skin vjs-big-play-centered"
+    controls
+    preload="auto"
+    width="100%"
+    height="auto"
+    poster="MY_VIDEO_POSTER.jpg"
+    data-setup="{}">
+    <source src="${videoUrl}" type="video/mp4">
+    <source src="${videoUrl}" type="video/webm">
+    </video>
+`;
+};
 
 export const TrailerMovie = ( videoUrl ) => {
-    const template =  `
-        <video
-        id="my-video"
-        class="video-js vjs-default-skin vjs-big-play-centered"
-        controls
-        preload="auto"
-        width="640"
-        height="268"
-        poster="MY_VIDEO_POSTER.jpg"
-        data-setup="{}">
-        <source src="${videoUrl}" type="video/mp4">
-        <source src="${videoUrl}" type="video/webm">
-        </video>
-    `;
+
+    return template(videoUrl);
 };
--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,11 +1,11 @@
 import { AssetDetailsDescription } from '../components/AssetDetails/Description.js';
 import { render } from '../shared/dom.js';
 import ApiProvider from '../providers/ApiProvider';
-import { TrailerMovie } from '../components/Trailer/trailer.js';
+import  { TrailerMovie }  from '../components/Trailer/trailer.js';
 
-const template = `
+const template = (trailer) => `
 <div id="details-screen" class="details">
-    <template class="details__trailer">${TrailerMovie}</template>
+    <template class="details__trailer">${trailer}</template>
     <template class="details__poster"></template>
     <template class="details__description"></template>
     <template class="details__gallery-button"></template>
@@ -18,7 +18,12 @@ export const DetailsScreen = async ({ renderOn }) => {
     const searchID = params.get('id');
     const movie = await ApiProvider.getTitleDetails(searchID);
 
-    render({ on: renderOn, html: template });
+    const trailer = '';
+    if (movie.Trailer && movie.Trailer.ThumbnailUrl) {
+        trailer = TrailerMovie(movie.Trailer.ThumbnailUrl);
+    }
+
+    render({ on: renderOn, html: template(trailer) });
 
     AssetDetailsDescription({ renderOn: '.details__description', movie });
 };
