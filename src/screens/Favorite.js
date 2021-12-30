f65c3a2a37969f131386975372376058889e250e
Konrad <trakon1e@gmail.com>
2021-12-30 11:20:42 +0100

    feat: Add NoFavorite component

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -2,10 +2,12 @@ import { Label } from '../components/Label/Label';
 import StorageManager from '../services/StorageManager';
 import { TilesGrid } from '../components/Tiles/TilesGrid';
 import { render } from '../shared/dom';
+import { NoFavorite } from '../components/NoFavorite/NoFavorite';
 
 const template = `
     <div>
         ${Label({ title: 'favorite' })}
+        <template id="no-favorite"></template>
         <template id="grid-template"></template>
     </div>
 `;
@@ -13,10 +15,12 @@ const template = `
 export const FavoriteScreen = ({ renderOn }) => {
     render({ html: template, on: renderOn });
     const favorites = StorageManager.getItem('favorites') || [];
-    if (favorites) {
+    if (favorites.length) {
         const favoritesToObj = favorites.map((movie) => ({
             id: movie
         }));
         TilesGrid({ renderOn: '#grid-template', movies: favoritesToObj });
+    } else {
+        NoFavorite({ renderOn: '#no-favorite' });
     }
 };
