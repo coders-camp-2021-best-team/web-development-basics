dcc568624c968172c0e34c592803d9c32cb32f2f
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-01-02 11:57:01 +0100

    changes

--- src/components/Nav/Link.js
+++ src/components/Nav/Link.js
@@ -2,7 +2,7 @@ export const NavLink = ({ href, title }) => {
     return `<li class="navbar-link"><a class="${isActive(href)}" href="${href}">${title}</a></li>`;
 };
 
-function isActive (href) {
+const isActive = (href) => {
     const activeUrl = window.location.pathname + window.location.search;
     return activeUrl === href ? 'active' : '';
 };
