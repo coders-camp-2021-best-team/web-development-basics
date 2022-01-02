3c9ec8378278ef26762c91f6682318aa2471e504
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-01-02 11:34:52 +0100

    change style for button carousel on mobile and comp

--- src/components/TitlesCarousel/TitlesCarousel.js
+++ src/components/TitlesCarousel/TitlesCarousel.js
@@ -74,8 +74,8 @@ export const MovieCarousel = async ({ movies, id, btnName, route }) => {
     <div id="carousel_prev-${id}" class="carousel__prev">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
+        width="42"
+        height="42"
         viewBox="0 0 24 24">
         <path fill="none" d="M0 0h24v24H0V0z" />
         <path d="M15.61 7.41L14.2 6l-6 6 6 6 1.41-1.41L11.03 12l4.58-4.59z" />
@@ -85,8 +85,8 @@ export const MovieCarousel = async ({ movies, id, btnName, route }) => {
     <div id="carousel_next-${id}" class="carousel__next">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
+        width="42"
+        height="42"
         viewBox="0 0 24 24">
         <path fill="none" d="M0 0h24v24H0V0z" />
         <path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z" />
